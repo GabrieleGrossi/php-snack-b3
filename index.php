fdb947bdb861f32c43feac68c68b2ee9b7728d3b
GabrieleGrossi <grossigabriele98@gmail.com>
2023-07-04 12:27:52 +0200

    create arrays

--- index.php
+++ index.php
@@ -1,3 +1,11 @@
+<!-- Scrivi una funzione che fonde due array (aventi lo stesso numero di elementi) prendendo alternativamente gli elementi da uno e dall'altro
+es. [a,b,c,d,e], [1,2,3,4,5] → [a,1,b,2,c,3,d,4,e,5]. -->
+
+<?php
+    $letters = ['a','b','c','d','e'];
+    $numbers = ['1','2','3','4','5'];
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
